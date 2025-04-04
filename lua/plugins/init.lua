b932e508324bc31e3b843108e6efc8d82bd6c8a4
Ken <ken@kenhui.me>
2025-04-04 10:03:38 +0800

    disable lsp by default

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -151,14 +151,14 @@ local plugins = {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = {
-          "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release",
-          "cmake --build build --config Release",
-          "cmake --install build --prefix build",
-        },
-      },
+      -- {
+      --   "nvim-telescope/telescope-fzf-native.nvim",
+      --   build = {
+      --     "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release",
+      --     "cmake --build build --config Release",
+      --     "cmake --install build --prefix build",
+      --   },
+      -- },
       "nvim-telescope/telescope-ui-select.nvim",
     },
     -- lazy = false,
